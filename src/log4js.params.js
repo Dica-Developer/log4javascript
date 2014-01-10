02ce50c025f1042f6d89c0f96662d21a63506033
Jörg Weber <weber.joerg.h@gmail.com>
2014-01-10 23:55:04 +0100

    * move globaly accessible params in extra file

---
+++ src/log4js.params.js
@@ -0,0 +1,11 @@
+/*jshint unused:false */
+var logLog,
+  applicationStartDate,
+  uniqueId,
+  emptyFunction,
+  newLine,
+  pageLoaded,
+  handleError,
+  enabled = true,
+  useTimeStampsInMilliseconds = true,
+  showStackTraces = false;
