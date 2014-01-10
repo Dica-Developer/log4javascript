b2cd866f3aa70d58c5d9925790c51571061143f4
Jörg Weber <weber.joerg.h@gmail.com>
2014-01-10 01:01:08 +0100

    * use rfc4122 version 4 compilant UUID generator

--- src/log4js.core.js
+++ src/log4js.core.js
@@ -91,8 +91,14 @@ EventSupport.prototype = {
 /* -------------------------------------------------------------------------- */
 
 var applicationStartDate = new Date();
-var uniqueId = "log4javascript_" + applicationStartDate.getTime() + "_" +
-  Math.floor(Math.random() * 100000000);
+var getUUID = function(){
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+};
+var uniqueId = "log4javascript_" + getUUID();
+console.log(uniqueId);
 var emptyFunction = function () {
 };
 var newLine = "\r\n";
