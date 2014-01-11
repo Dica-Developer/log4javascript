0b9bc290d7ff85021b122185f5ea7f1016dcf6e1
Jörg Weber <weber.joerg.h@gmail.com>
2014-01-11 17:54:35 +0100

    * add jsDoc for JsonFormatter

--- src/log4js.layout.json.js
+++ src/log4js.layout.json.js
@@ -1,40 +1,82 @@
-/* ---------------------------------------------------------------------- */
-// JsonLayout related
-
+/**
+ *
+ * @param {String} str
+ * @returns {*|XML|string|Node|void}
+ */
 function escapeNewLines(str) {
-  return str.replace(/\r\n|\r|\n/g, "\\r\\n");
+  'use strict';
+
+  return str.replace(/\r\n|\r|\n/g, '\\r\\n');
 }
 
+/**
+ * JsonLayout
+ * @param readable
+ * @param combineMessages
+ * @constructor
+ * @property {Boolean} readable
+ * @property {Boolean} combineMessages
+ * @property {undefined | String} batchHeader
+ * @property {undefined | String} batchFooter
+ * @property {undefined | String} batchSeparator
+ * @property {String} colon
+ * @property {String} tab
+ * @property {String} newLine
+ * @property {Array} customFields
+ * @mixes Layout
+ */
 function JsonLayout(readable, combineMessages) {
+  'use strict';
+
   this.readable = extractBooleanFromParam(readable, false);
   this.combineMessages = extractBooleanFromParam(combineMessages, true);
-  this.batchHeader = this.readable ? "[" + newLine : "[";
-  this.batchFooter = this.readable ? "]" + newLine : "]";
-  this.batchSeparator = this.readable ? "," + newLine : ",";
+  this.batchHeader = this.readable ? '[' + newLine : '[';
+  this.batchFooter = this.readable ? ']' + newLine : ']';
+  this.batchSeparator = this.readable ? ',' + newLine : ',';
   this.setKeys();
-  this.colon = this.readable ? ": " : ":";
-  this.tab = this.readable ? "\t" : "";
-  this.lineBreak = this.readable ? newLine : "";
+  this.colon = this.readable ? ': ' : ':';
+  this.tab = this.readable ? '\t' : '';
+  this.lineBreak = this.readable ? newLine : '';
   this.customFields = [];
 }
 
-/* ---------------------------------------------------------------------- */
-// JsonLayout
-
+/**
+ *
+ * @type {Layout}
+ */
 JsonLayout.prototype = new Layout();
 
+/**
+ *
+ * @returns {Boolean}
+ */
 JsonLayout.prototype.isReadable = function () {
+  'use strict';
+
   return this.readable;
 };
 
+/**
+ *
+ * @returns {Boolean}
+ */
 JsonLayout.prototype.isCombinedMessages = function () {
+  'use strict';
+
   return this.combineMessages;
 };
 
+/**
+ *
+ * @param loggingEvent
+ * @returns {String}
+ */
 JsonLayout.prototype.format = function (loggingEvent) {
+  'use strict';
+
   var layout = this;
   var dataValues = this.getDataValues(loggingEvent, this.combineMessages);
-  var str = "{" + this.lineBreak;
+  var str = '{' + this.lineBreak;
   var i, len;
 
   function formatValue(val, prefix, expand) {
@@ -45,18 +87,18 @@ JsonLayout.prototype.format = function (loggingEvent) {
     if (val instanceof Date) {
       formattedValue = String(val.getTime());
     } else if (expand && (val instanceof Array)) {
-      formattedValue = "[" + layout.lineBreak;
+      formattedValue = '[' + layout.lineBreak;
       for (var i = 0, len = val.length; i < len; i++) {
         var childPrefix = prefix + layout.tab;
         formattedValue += childPrefix + formatValue(val[i], childPrefix, false);
         if (i < val.length - 1) {
-          formattedValue += ",";
+          formattedValue += ',';
         }
         formattedValue += layout.lineBreak;
       }
-      formattedValue += prefix + "]";
-    } else if (valType !== "number" && valType !== "boolean") {
-      formattedValue = "\"" + escapeNewLines(toStr(val).replace(/\"/g, "\\\"")) + "\"";
+      formattedValue += prefix + ']';
+    } else if (valType !== 'number' && valType !== 'boolean') {
+      formattedValue = '"' + escapeNewLines(toStr(val).replace(/\'/g, '\\\"')) + '"';
     } else {
       formattedValue = val;
     }
@@ -64,27 +106,49 @@ JsonLayout.prototype.format = function (loggingEvent) {
   }
 
   for (i = 0, len = dataValues.length - 1; i <= len; i++) {
-    str += this.tab + "\"" + dataValues[i][0] + "\"" + this.colon + formatValue(dataValues[i][1], this.tab, true);
+    str += this.tab + '"' + dataValues[i][0] + '"' + this.colon + formatValue(dataValues[i][1], this.tab, true);
     if (i < len) {
-      str += ",";
+      str += ',';
     }
     str += this.lineBreak;
   }
 
-  str += "}" + this.lineBreak;
+  str += '}' + this.lineBreak;
   return str;
 };
 
+/**
+ *
+ * @returns {Boolean}
+ */
 JsonLayout.prototype.ignoresThrowable = function () {
+  'use strict';
+
   return false;
 };
 
+/**
+ *
+ * @returns {String}
+ */
 JsonLayout.prototype.toString = function () {
-  return "JsonLayout";
+  'use strict';
+
+  return 'JsonLayout';
 };
 
+/**
+ *
+ * @returns {String}
+ */
 JsonLayout.prototype.getContentType = function () {
-  return "application/json";
+  'use strict';
+
+  return 'application/json';
 };
 
+/**
+ *
+ * @type {JsonLayout}
+ */
 log4javascript.JsonLayout = JsonLayout;
