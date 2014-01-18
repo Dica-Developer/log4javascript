f9020bcbd6652adc1368171b4efe859809990829
Jörg Weber <weber.joerg.h@gmail.com>
2014-01-18 01:30:28 +0100

    * add appenderSpec file

--- test/appenderSpec.js
+++ test/appenderSpec.js
@@ -1,3 +1,17 @@
-/**
- * Created by jweber on 17.01.14.
- */
+/*global define, describe, it, xit, expect, spyOn, beforeEach*/
+define(['params', 'level', 'logger', 'eventSupport', 'appender'], function () {
+  'use strict';
+
+  describe('#Appender', function () {
+
+    var appender = null;
+
+    beforeEach(function () {
+      appender = new Appender();
+    });
+
+    it('All #entryPoints should be true', function () {
+
+    });
+  });
+});
