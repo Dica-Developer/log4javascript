442d96ba7c7db8fe972580fa467bdbeb9f8b0f17
Jörg Weber <weber.joerg.h@gmail.com>
2014-04-24 15:36:04 +0200

    * adjust Grunt tasks to new architecture

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,64 +15,23 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     config: config,
+    watch: {
+      options: {
+        nospawn: true
+      },
+      dev: {
+        files: [
+          '<%= config.src %>/**/*.js'
+        ],
+        tasks: ['devWatch']
+      }
+    },
     clean: {
       dist: {
         files: [{
           dot: true,
           src: ['<%= config.dist %>/*']
         }]
-      }
-    },
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      afterconcat:  ['<%= config.dist %>/log4javascript.js'],
-      beforeconcat:  ['<%= config.src %>/*.js']
-    },
-    copy: {
-      sample: {
-        files: [{
-          expand: true,
-          cwd: '<%= config.app %>',
-          dest: '<%= config.dist %>/app.nw',
-          src: '**'
-        }]
-      }
-    },
-    concat: {
-      options: {},
-      dist: {
-        options: {
-          banner: 'define(\'log4javascript\', [], function() {\n \'use strict\';\n ',
-          footer: '\nreturn log4javascript;\n});'
-        },
-        src: [
-          'src/log4js.params.js',
-          'src/log4js.formatObjectExpansion.js',
-          'src/log4js.simpleDateFormat.js',
-          'src/log4js.level.js',
-          'src/log4js.logger.js',
-          'src/log4js.eventSupport.js',
-          'src/log4js.core.js',
-          'src/log4js.layout.js',
-          'src/log4js.layout.httpPostData.js',
-          'src/log4js.layout.null.js',
-          'src/log4js.layout.json.js',
-          'src/log4js.layout.pattern.js',
-          'src/log4js.layout.simple.js',
-          'src/log4js.layout.xml.js',
-          'src/log4js.appender.js',
-          'src/log4js.appender.ajax.js',
-          'src/log4js.appender.alert.js',
-          'src/log4js.appender.browserConsole.js',
-          'src/log4js.consoleHtmlLines.js',
-          'src/log4js.appender.console.js',
-          'src/log4js.appender.popup.js',
-          'src/log4js.appender.inPage.js',
-          'src/log4js.popupInpage.init.js'
-        ],
-        dest: 'dist/log4javascript.js'
       }
     },
     uglify: {
@@ -83,12 +42,32 @@ module.exports = function (grunt) {
       }
     },
     jsdoc : {
-      dev : {
-        src: ['src/*.js'],
+      dist : {
+        src: ['<%= config.lib %>/*.js', '<%= config.lib %>/api/*.js', 'README.md'],
         options: {
-          destination: 'documentation'
+          destination: 'doc',
+          configure: 'jsdoc.conf.json',
+          template: 'node_modules/grunt-jsdoc/node_modules/ink-docstrap/template',
+          tutorials: 'tutorials'
         }
       }
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      files: ['<%= config.src %>/*.js']
+    },
+    requirejs: {
+      options: {
+        loglevel: 5,
+        inlineText: true,
+        baseUrl: './<%= config.src %>',
+        optimize: 'uglify',
+        name: 'log4js',
+        out: 'dist/log4js.min.js'
+      },
+      dist: {}
     },
     karma: {
       dist: {
@@ -100,19 +79,25 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('check', [
+  grunt.registerTask('devWatch', [
     'jshint'
   ]);
 
+  grunt.registerTask('dev', [
+    'jshint',
+    'watch:dev'
+  ]);
+
   grunt.registerTask('build', [
-    'jshint:beforeconcat',
+    'jshint',
     'clean:dist',
     'concat:dist',
     'karma:dist',
     'uglify:dist'
   ]);
 
-  grunt.registerTask('dev', [
+  grunt.registerTask('test', [
+    'jshint',
     'karma:dev'
   ]);
 };
