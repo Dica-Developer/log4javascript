8953f771e2ed3b6383d3fdd89df91912afa416ec
Jörg Weber <weber.joerg.h@gmail.com>
2014-04-25 16:09:32 +0200

    * add dist and build files for browser console

--- src/log4js.part.console.js
+++ src/log4js.part.console.js
@@ -1,4 +1,4 @@
-define(['log4js.core', 'log4js.appender.browserConsole', 'log4js.layout.json'], function(log4js){
+define(['log4js.core', 'log4js.appender.browserConsole'], function(log4js){
   'use strict';
   return log4js;
 });
