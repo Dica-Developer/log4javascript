62c3fa23280f696d4028562f0075f2d62ff91568
Jörg Weber <weber.joerg.h@gmail.com>
2014-04-26 16:05:06 +0200

    * add getUUID method to helper

--- src/log4js.helper.js
+++ src/log4js.helper.js
@@ -10,6 +10,12 @@ define(function () {
   };
 
   Helper.prototype = {
+    getUUID: function getUUID(){
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+      });
+    },
     isUndefined: function isUndefined(obj) {
       return obj === void 0;
     },
