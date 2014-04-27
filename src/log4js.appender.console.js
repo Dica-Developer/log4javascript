cefce0eeadb54d54b5c44a07f0b3a10596926ac9
Jörg Weber <weber.joerg.h@gmail.com>
2014-04-27 23:41:22 +0200

    * code formatting

--- src/log4js.appender.console.js
+++ src/log4js.appender.console.js
@@ -1,45 +1,45 @@
 /*global window*/
-define(['log4js.helper', 'log4js.core', 'log4js.consoleAppenderHtml', 'log4js.appender', 'log4js.layout.pattern'], function(helper, log4js, consoleAppenderHtml){
+define(['log4js.helper', 'log4js.core', 'log4js.consoleAppenderHtml', 'log4js.appender', 'log4js.layout.pattern'], function (helper, log4js, consoleAppenderHtml) {
   'use strict';
 
-  function LogCache(size){
+  function LogCache(size) {
     this.size = size || 100;
     this.cache = [];
   }
 
-  LogCache.prototype.setSize = function(newSize){
+  LogCache.prototype.setSize = function (newSize) {
     this.size = newSize;
   };
 
-  LogCache.prototype.add = function(loggingEvent){
-    if(this.cache.length === this.size){
+  LogCache.prototype.add = function (loggingEvent) {
+    if (this.cache.length === this.size) {
       this.cache.shift();
     }
 
     this.cache.push(loggingEvent);
   };
 
-  LogCache.prototype.getAtIndex = function(index){
+  LogCache.prototype.getAtIndex = function (index) {
     return this.cache[index];
   };
 
-  LogCache.prototype.getFirst = function(){
+  LogCache.prototype.getFirst = function () {
     return this.getAtIndex(0);
   };
 
-  LogCache.prototype.getLast = function(){
+  LogCache.prototype.getLast = function () {
     return this.getAtIndex(this.cache.length);
   };
 
-  LogCache.prototype.get = function(){
+  LogCache.prototype.get = function () {
     return this.cache;
   };
 
-  LogCache.prototype.clear = function(){
+  LogCache.prototype.clear = function () {
     this.cache = [];
   };
 
-  function ConsoleAppender(options){
+  function ConsoleAppender(options) {
     options = options || {};
     this.options = {
       inPage: options.inPage || true,
@@ -50,7 +50,7 @@ define(['log4js.helper', 'log4js.core', 'log4js.consoleAppenderHtml', 'log4js.ap
 
   ConsoleAppender.prototype = new log4js.Appender();
 
-  ConsoleAppender.prototype.create = function(){
+  ConsoleAppender.prototype.create = function () {
 
     this.setLayout(this.options.layout);
 
@@ -61,14 +61,14 @@ define(['log4js.helper', 'log4js.core', 'log4js.consoleAppenderHtml', 'log4js.ap
 
     this.prepareContainer();
 
-    if(this.options.inPage){
+    if (this.options.inPage) {
       this.addContainerToPage();
     } else {
       this.addContainerToPopup();
     }
   };
 
-  ConsoleAppender.prototype.prepareContainer = function(){
+  ConsoleAppender.prototype.prepareContainer = function () {
     this.mainContainer.style.position = 'fixed';
     this.mainContainer.style.left = '0';
     this.mainContainer.style.right = '0';
@@ -82,41 +82,41 @@ define(['log4js.helper', 'log4js.core', 'log4js.consoleAppenderHtml', 'log4js.ap
     this.logContainer.innerHTML = consoleAppenderHtml;
   };
 
-  ConsoleAppender.prototype.addContainerToPage = function(){
+  ConsoleAppender.prototype.addContainerToPage = function () {
     window.document.body.appendChild(this.mainContainer);
     this.logContainer.appendChild(this.logDiv);
     this.mainContainer.appendChild(this.logContainer);
   };
 
-  ConsoleAppender.prototype.addContainerToPopup = function(){
+  ConsoleAppender.prototype.addContainerToPopup = function () {
     window.document.body.appendChild(this.mainContainer);
     this.mainContainer.appendChild(this.logContainer);
   };
 
-  ConsoleAppender.prototype.renderLogs = function(){
+  ConsoleAppender.prototype.renderLogs = function () {
     var layout = this.layout;
     var logs = this.logCache.get();
     var logEntries = [];
-    logs.forEach(function(loggingEvent){
+    logs.forEach(function (loggingEvent) {
       logEntries.push('<div>' + layout.format(loggingEvent) + '</div>');
     });
     this.logDiv.innerHTML = logEntries.join('');
   };
 
-  ConsoleAppender.prototype.doAppend = function(loggingEvent){
+  ConsoleAppender.prototype.doAppend = function (loggingEvent) {
     this.logCache.add(loggingEvent);
     this.renderLogs();
   };
 
-  ConsoleAppender.prototype.group = function(){
+  ConsoleAppender.prototype.group = function () {
 
   };
 
-  ConsoleAppender.prototype.groupEnd = function(){
+  ConsoleAppender.prototype.groupEnd = function () {
 
   };
 
-  ConsoleAppender.prototype.toString = function(){
+  ConsoleAppender.prototype.toString = function () {
     return 'ConsoleAppender';
   };
 
